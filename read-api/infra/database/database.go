d0734b8deed200d42cb726494b67060eae651885
reivosar <users.noreply.github.com>
2022-08-11 07:57:51 +0900

    move package

---
+++ read-api/infra/database/database.go
@@ -0,0 +1,19 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
+
+func getDBConnection() *sql.DB {
+	user := os.Getenv("MYSQL_USER")
+	pw := os.Getenv("MYSQL_PASSWORD")
+	db_name := os.Getenv("MYSQL_DATABASE")
+	var path string = fmt.Sprintf("%s:%s@tcp(db-container:3306)/%s?charset=utf8", user, pw, db_name)
+	db, err := sql.Open("mysql", path)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
